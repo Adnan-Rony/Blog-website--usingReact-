c170b5083fa648c3d764d432a1133ab5bd7b9a95
adnanrony <adnanrony19@gmail.com>
2024-04-01 17:54:30 +0600

    signin and signup page functionaly setup done

--- Front-End/Front-end-bolg/src/components/InputBox.jsx
+++ Front-End/Front-end-bolg/src/components/InputBox.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import { GoEyeClosed } from "react-icons/go";
+import { FaEye } from "react-icons/fa";
 
-const InputBox = ({ name, id, value, placeholder, type, icon }) => {
+
+const InputBox = ({ name, id, value, placeholder, type, icon,done }) => {
+  
+  const [passwordvisiable, setpassowrdvisiable] = useState(false);
+
+
   return (
-    <div className="relative w-[100%] mb-4 ">
+    <div className="relative  w-[100%] mb-4 ">
       <input
         className="input ml-3 w-full bg-slate-50 border-gray-100 rounded-xl pl-10  outline-none border-none"
         name={name}
         id={id}
-        type={type}
+        type={
+          type == "password" ? (passwordvisiable ? "text" : "password") : type
+        }
         placeholder={placeholder}
         defaultValue={value}
       />
+
       <div className="absolute top-3 left-4 text-2xl">{icon}</div>
+
       {type == "password" ? (
         <div className="absolute cursor-pointer top-3 right-4 text-2xl">
-          <GoEyeClosed />
+          <FaEye
+            onClick={() => setpassowrdvisiable((currentvalue) => !currentvalue)}
+          />
         </div>
       ) : (
         ""
       )}
+
+         
+
     </div>
   );
 };
--- Front-End/Front-end-bolg/src/components/UseAuthForm.jsx
+++ Front-End/Front-end-bolg/src/components/UseAuthForm.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import InputBox from "./InputBox";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 const UseAuthForm = ({ type }) => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <form>
+    <div className="h-screen   flex items-center justify-center">
+      <form className=" ">
         <p className="text-4xl font-semibold capitalize py-10 my-5">
           {type === "sign-in" ? "Welcome back" : "Join us today"}
         </p>
@@ -33,6 +35,39 @@ const UseAuthForm = ({ type }) => {
           placeholder="password"
           icon={<RiLockPasswordLine />}
         />
+        <button
+        type="submit"
+          className="btn btn-sm border-none mx-auto rounded-full   flex  bg-black text-white btn-outline"
+          
+        >
+          {type.replace("-"," ")}
+        </button>
+        <div className="relative w-full flex items-center justify-center gap-3 my-5 opacity-10 uppercase text-black font-bold">
+          <hr className="w-1/2 border-black" /> 
+          <p>or</p>
+          <hr className="w-1/2 border-black" />
+
+
+        </div>
+        <button className="btn border-none bg-black w-full rounded-full hover:bg-[#4285F4] text-white">
+           <FcGoogle className="text-2xl" />continue with google
+        </button>
+       <div className="my-5">
+       {
+          type=="sign-in" ?
+          <p className="text-gray-500">Don't have an account ? 
+          <Link className="link" to="/signup">
+             join us today
+          </Link>
+          </p>
+          :
+          <p> already a member ?
+          <Link className="link" to="/signin">
+             sign in here
+          </Link> </p>
+        }
+       </div>
+
       </form>
     </div>
   );
